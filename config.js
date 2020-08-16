aa165ab45fc855209e4422da53a86546bb92daf8
Rokan Uddin <rokanu55@gmail.com>
2020-08-16 09:11:15 +0600

    User authentication by Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+    	clientId : '1534725086706856',
+    	clientSecret : 'cec46295d65cc74cc278fe5d5a717699'
+    }
 }
